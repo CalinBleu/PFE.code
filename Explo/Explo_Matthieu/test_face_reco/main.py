4cf1e89e0ed8af7e8660cd41a1aba724f7d37d5a
Matthieu Bonhomme <matthieu.bonhomme.72@gmail.com>
2023-11-30 18:07:32 +0100

    CREATE - Ajout des explo de reconnaissance faciale sur le git

---
+++ Explo/Explo_Matthieu/test_face_reco/main.py
@@ -0,0 +1,33 @@
+
+import cv2
+import numpy as np
+from picamera2 import Picamera2
+
+name = 'Matthieu' #replace with your name
+
+cam = Picamera2()
+config = cam.create_preview_configuration(main={"size":(512, 304), "format":"RGB888"})
+cam.configure(config)
+cam.start()
+    
+img_counter = 0
+
+while True:
+    
+    image = cam.capture_array("main")
+    cv2.imshow("Press Space to take a photo", image)
+
+    k = cv2.waitKey(1)
+
+    if k%256 == 32:
+        # SPACE pressed
+        img_name = "dataset/"+ name +"/image_{}.jpg".format(img_counter)
+        cv2.imwrite(img_name, image)
+        print("{} written!".format(img_name))
+        img_counter += 1
+        
+    elif k%256 == 27: # ESC pressed
+        print("Escape hit, closing...")
+        break
+
+cv2.destroyAllWindows()
