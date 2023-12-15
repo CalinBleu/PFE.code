faf53897ef8e6d12ea6d44e6bef3c553749ad40a
juganval <valentin.jugan@reseau.eseo.fr>
2023-12-15 11:28:19 +0100

    MAJ - Doorman

---
+++ Explo/Explo_Valentin/doorman.h
@@ -0,0 +1,18 @@
+/**
+ * brief Fonction permettant d'ouvrir la porte
+ * 
+ */
+void open();
+
+/**
+ * brief Fonction permettant l'allumage de la LED correspondant à l'évènement "l'utilisateur est 
+ * reconnu mais n'a pas le droit d'accéder au bâtiment"
+ * 
+ */
+void Doorman_userDenied();
+
+/**
+ * brief Fonction permettant l'allumage de la LED correspondant à l'évènement "l'utilisateur n'est pas reconnu"
+ * 
+ */
+void Doorman_userUnknown();
