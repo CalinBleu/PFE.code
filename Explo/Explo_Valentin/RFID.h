17dcb648d02324d45297c9029ca10271551c2267
juganval <valentin.jugan@reseau.eseo.fr>
2023-12-15 15:03:06 +0100

    CREATE - RFID.h

---
+++ Explo/Explo_Valentin/RFID.h
@@ -0,0 +1,18 @@
+
+/**
+ * brief Fonction permettant de mettre en marche la lecture d'un badge RFID
+ * 
+ */
+void RFID_stopReading();
+
+/**
+ * brief Fonction permettant de lire un tag présenté au lecteur
+ * 
+ */
+void RFID_showTag();
+
+/**
+ * brief Fonction permettant de stopper la lecture après le passage d'un badge RFID
+ * 
+ */
+void RFID_startReading();
