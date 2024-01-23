e3fbc2539afd62666229662a94618f5ed3feb7b0
Matéo RONDEAU <mateo.rondeau@reseau.eseo.fr>
2024-01-23 15:54:49 +0100

    CERATE- Ajout de gui

--- Production/src/gui/gui.h
+++ Production/src/gui/gui.h
@@ -1,64 +1,150 @@
 #ifndef GUI_H
 #define GUI_H
 
-void GUI_screenOn();
-
-void GUI_screenOff();
-
-void GUI_askAddUser();
-
-void GUI_askModifyUser(User user);
-
-void GUI_askDeleteUser(User user);
-
-void GUI_setSearchField(char* search);
-
-void GUI_validate();
-
-void GUI_cancel();
-
-void GUI_setFieldPassword(char* password);
-
-void GUI_validatePassword(bool result);
-
-void GUI_setName(char* name);
-
-void GUI_setFirstName(char* firstName);
-
-void GUI_setRole(Role role);
-
-void GUI_setAccess(Access access);
-
-void GUI_setTag(char* idTag);
-
-void GUI_setPicture(Picture picture);
-
-void GUI_setPassword(char* password);  
-
-void GUI_askTakePicture();
-
-void GUI_adminMode();
-
-void GUI_quitAdminMode();
-
-void GUI_displayHomeScreen(AuthResult result);
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 /**
- * @brief Fonction d'initialisation de l'objet GUI
+ * @brief Méthode publique permettant l’allumage de l’écran.
  * 
  */
-int GUI_new();
+void void Gui_screenOn();
 
 /**
- * @brief Fonction de démarrage de l'objet GUI
+ * @brief Méthode publique permettant d’éteindre l’écran.
  * 
  */
-int GUI_start();
+void Gui_screenOff();
 
 /**
- * @brief Fonction de destruction de l'objet GUI
+ * @brief Méthode publique permettant de faire la demande d’ajout d’un nouvel Utilisateur.
  * 
  */
-int GUI_free();
+void Gui_askAddUser();
 
-#endif //GUI_H
+/**
+ * @brief Méthode publique permettant de faire la demande de modification d’un utilisateur de la base de données.
+ * 
+ */
+void Gui_askModifyUser(User user);
+
+/**
+ * @brief Méthode publique permettant de faire la demande de supprimer un Utilisateur de la base de données.
+ * 
+ */
+void Gui_askDeleteUser(User user);
+
+/**
+ * @brief Méthode publique permettant d’inscrire un nom d’Utilisateur dans le champ de recherche.
+ * 
+ */
+void Gui_setSearchField(char* search);
+
+/**
+ * @brief Méthode publique permettant de valider une action.
+ * 
+ */
+void Gui_validate();
+
+/**
+ * @brief Méthode publique permettant d’annuler une action.
+ * 
+ */
+void Gui_cancel();
+
+/**
+ * @brief Méthode publique permettant d’inscrire un mot de passe dans le champ mot de passe.
+ * 
+ */
+void Gui_setFieldPassword(char* password);
+
+/**
+ * @brief Méthode publique permettant de valider le mot de passe inscrit dans le champ mot de passe.
+ * 
+ */
+void Gui_validatePassword(bool result);
+
+/**
+ * @brief Méthode publique permettant d’inscrire un nouveau nom d’Utilisateur dans le champ nom utilisateur.
+ * 
+ */
+void Gui_setName(char* name);
+
+/**
+ * @brief Méthode publique permettant d’inscrire un nouveau prénom d’Utilisateur dans le champ prénom utilisateur.
+ * 
+ */
+void Gui_setFirstName(char* firstname);
+
+/**
+ * @brief Méthode publique permettant de choisir le rôle voulu pour le nouvel Utilisateur.
+ * 
+ */
+void Gui_setRole(Role role);
+
+/**
+ * @brief Méthode publique permettant de choisir les accès autorisés du nouvel Utilisateur.
+ * 
+ */
+void Gui_setAccess(Access access);
+
+/**
+ * @brief Méthode publique permettant d’attribuer un tag RFID au nouvel Utilisateur.
+ * 
+ */
+void Gui_setTag(char* idTag);
+
+/**
+ * @brief Méthode publique permettant d’ajouter une photo du nouvel Utilisateur.
+ * 
+ */
+void Gui_setPicture(Picture picture);
+
+/**
+ * @brief Méthode publique permettant de faire la demande de prise d’une photo.
+ * 
+ */
+void Gui_askTakePicture();
+
+/**
+ * @brief Méthode publique permettant de basculer vers le mode administrateur.
+ * 
+ */
+void Gui_adminMode();
+
+/**
+ * @brief Méthode publique permettant de quitter le mode administrateur.
+ * 
+ */
+void Gui_quitAdminMode();
+
+/**
+ * @brief Méthode publique permettant d’afficher les écrans d’authentification. 
+ * 
+ */
+void Gui_displayHomeScreen(AuthResult result);
+
+/**
+ * @brief Fonction d'initialisation de l'objet Gui. 
+ * 
+ */
+int Gui_new();
+
+/**
+ * @brief Fonction de démarrage de l'objet Gui. 
+ * 
+ */
+int Gui_start();
+
+/**
+ * @brief Fonction de destruction de l'objet Gui. 
+ * 
+ */
+int Gui_free();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // GUI_H
